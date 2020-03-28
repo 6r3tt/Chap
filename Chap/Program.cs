b86f7b315003112810bed2783a2ca56464757516
Brett Christen <brett.christen08@gmail.com>
2020-03-28 15:24:48 -0600

    Added MainWindow with the title Chap.

--- Chap/Program.cs
+++ Chap/Program.cs
@@ -16,7 +16,6 @@ namespace Chap
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+            Application.Run(new MainWindow());        }
     }
 }
